0e77d8eae02399061cbc7ea0f8af9d9ba1d74b6e
hellodword <46193371+hellodword@users.noreply.github.com>
2023-04-14 15:03:49 +0800

    rename bundle id and app name

---
+++ rename.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+set -e
+set -x
+
+APP_NAME='Search Magic'
+
+flutter pub global run rename --bundleId hellodword.search_magic
+
+flutter pub global run rename --appname "$APP_NAME" --target ios
+flutter pub global run rename --appname "$APP_NAME" --target android
+flutter pub global run rename --appname "$APP_NAME" --target web
+flutter pub global run rename --appname "$APP_NAME" --target macOS
+flutter pub global run rename --appname "$APP_NAME" --target windows
+flutter pub global run rename --appname "$APP_NAME" --target linux
